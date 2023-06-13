5f64d0825e22d454e38dca8363e11d41b72ab65c
DTS1234 <adas.kazmierczak@gmail.com>
2023-06-13 16:54:12 +0200

    Refactor comparator

--- src/main/java/uni/trento/probebuilder/jmeter/JmeterRepository.java
+++ src/main/java/uni/trento/probebuilder/jmeter/JmeterRepository.java
@@ -1,6 +1,6 @@
 package uni.trento.probebuilder.jmeter;
 
-import net.sf.saxon.expr.parser.Loc;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -9,7 +9,6 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.UUID;
 
 @Component
 public class JmeterRepository {
@@ -22,20 +21,29 @@ public class JmeterRepository {
 
     public Map<String, JmeterResultData> getResults() {
 
-        Comparator<String> comparator = (String s1, String s2) -> {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            boolean after = LocalDateTime.parse(s1, formatter).isBefore(LocalDateTime.parse(s2, formatter));
+        Comparator<String> comparator = getDateFromStringComparator();
 
-            if (after) {
-                return 1;
-            } else {
-                return -1;
-            }
-        };
-
-        Map sortedResults = new TreeMap<>(comparator);
+        Map<String, JmeterResultData> sortedResults = new TreeMap<>(comparator);
         sortedResults.putAll(resultsMap);
 
         return sortedResults;
+    }
+
+    @NotNull
+    private static Comparator<String> getDateFromStringComparator() {
+        Comparator<String> comparator = (String s1, String s2) -> {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            boolean isBefore = LocalDateTime.parse(s1, formatter).isBefore(LocalDateTime.parse(s2, formatter));
+            boolean isAfter = LocalDateTime.parse(s1, formatter).isAfter(LocalDateTime.parse(s2, formatter));
+
+            if (isBefore) {
+                return 1;
+            } else if (isAfter) {
+                return -1;
+            } else {
+                return 0;
+            }
+        };
+        return comparator;
     }
 }
